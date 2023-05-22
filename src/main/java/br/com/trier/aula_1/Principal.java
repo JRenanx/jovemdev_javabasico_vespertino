a701070b0f62968b4b9a5569a0e357808413fe83
Renan Silva <renan_lg@live.com>
2023-05-22 15:51:09 -0300

    segundo commit

--- src/main/java/br/com/trier/aula_1/Principal.java
+++ src/main/java/br/com/trier/aula_1/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 
     public static void main(String[] args) {
         
-        System.out.println("Olá Mundo!");
+        System.out.println("Olá Mund!");
+        System.out.println("************");
     }
 }
